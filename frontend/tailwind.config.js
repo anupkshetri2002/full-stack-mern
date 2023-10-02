f0a60ac0963f6ca6323c866abe55e27241bf4e44
anupkshetri2002 <anupac2002@gmail.com>
2023-10-02 18:35:01 +0545

    signup using signup button

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-
-      
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+  }
